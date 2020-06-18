bb762dd2837c8c78b76ae9a1626218958f01c21f
asya2307 <yetymoye@gmail.com>
2020-06-18 07:52:57 +0500

    fix

--- src/js/partials/special.js
+++ src/js/partials/special.js
@@ -6,13 +6,22 @@ document.addEventListener('DOMContentLoaded', (e) => {
                  new Swiper(item, {
                     speed: 1200,
                     spaceBetween: 150,
-                    slidesPerView: 3,
+                    slidesPerView: 1,
                     autoHeight: true,
                     loop: true,
                     navigation: {
                         nextEl: ".slider__nav-item--next",
                         prevEl: ".slider__nav-item--prev"
-                    }
+                    },
+                    breakpoints: {
+                        // when window width is >= 320px
+                        768: {
+                          slidesPerView: 3,
+                        },
+                        1024: {
+                            slidesPerView: 3,
+                        }
+                      }
                 })
         })
     }
